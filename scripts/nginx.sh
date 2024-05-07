1a769a04fa6814564e4a872fa28bfaa8801bc602
DhruvSutharJobs <162339786+DhruvSutharJobs@users.noreply.github.com>
2024-05-08 00:50:57 +0530

    Update nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,10 +4,10 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/blogprojectdrf/nginx/nginx.conf /etc/nginx/sites-available/nginx.conf
-sudo ln -s /etc/nginx/sites-available/nginx.conf /etc/nginx/sites-enabled/nginx.conf
-# sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
-# sudo nginx -t
+sudo cp /home/ubuntu/blogprojectdrf/nginx/nginx.conf /etc/nginx/sites-available/fodler_name_where_seetngs.py_is
+sudo ln -s /etc/nginx/sites-available/fodler_name_where_seetngs.py_is /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
+sudo nginx -t
 sudo gpasswd -a www-data ubuntu
 sudo systemctl restart nginx
 
